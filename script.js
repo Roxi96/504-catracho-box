ac08bf0422ee13b06098ed5669167539edae17cb
Roxi96 <roxnazapc@gmail.com>
2021-07-13 15:01:45 -0600

    galeria terminada

--- script.js
+++ script.js
@@ -25,4 +25,17 @@ $(document).ready(function () {
         }
     });
 
+    $('.gallery-list-item').click(function(){
+    let value = $(this).attr('data-filter');
+    if(value === 'todos'){
+        $('.filter').show(300);
+    }else{
+        $('.filter').not('.'+value).hide(300);
+        $('.filter').filter('.'+ value).show(300);
+    }
+    });
+
+    $('.gallery-list-item').click(function(){
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
 });
